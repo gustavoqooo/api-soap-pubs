597b941107d72b4bb78f66b5e1a97af08e15b9df
gustavoqooo <gustavoqooo20@gmail.com>
2024-06-02 09:27:19 -0500

    ws paises terminado

--- src/main/java/pe/edu/cibertec/api_soap_pubs/configuration/WebServiceConfig.java
+++ src/main/java/pe/edu/cibertec/api_soap_pubs/configuration/WebServiceConfig.java
@@ -1,4 +1,42 @@
 package pe.edu.cibertec.api_soap_pubs.configuration;
 
-public class WebServiceConfig {
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet
+            (ApplicationContext applicationContext){
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+    @Bean(name = "paises")
+    public DefaultWsdl11Definition paisWsdl11Definition(XsdSchema paisSchema){
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("PaisesPort");
+        wsdl11Definition.setLocationUri("/ws/paises");
+        wsdl11Definition.setTargetNamespace("http://www.cibertec.edu.pe/ws/objects");
+        wsdl11Definition.setSchema(paisSchema);
+        return wsdl11Definition;
+    }
+    @Bean
+    public XsdSchema paisSchema(){
+        return new SimpleXsdSchema(new ClassPathResource("xsd/paises.xsd"));
+    }
+
+
 }
